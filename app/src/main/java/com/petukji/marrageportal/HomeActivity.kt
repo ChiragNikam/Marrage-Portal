84d5533269d5094cfcbcaa3e2b82302504669dc1
ChiargNikam <chiragnikam02@gmail.com>
2024-02-29 14:28:34 +0530

    bugfix - fixed visiblity for bottom sheet at home

--- app/src/main/java/com/petukji/marrageportal/HomeActivity.kt
+++ app/src/main/java/com/petukji/marrageportal/HomeActivity.kt
@@ -40,13 +40,18 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -69,6 +74,7 @@ import com.petukji.marrageportal.Views.RequestReceivedScreen
 import com.petukji.marrageportal.Views.SearchScreen
 import com.petukji.marrageportal.Views.SearchViewBottomSheet
 import com.petukji.marrageportal.ui.theme.MarriagePortalTheme
+import kotlinx.coroutines.launch
 
 class HomeActivity : ComponentActivity() {
 
@@ -110,6 +116,7 @@ class HomeActivity : ComponentActivity() {
             // nav controller
             val navController = rememberNavController()
 
+            val currentDestination by homeViewModel.currentDestinationBottomNav.collectAsState()
 
             MarriagePortalTheme {
                 // A surface container using the 'background' color from the theme
@@ -125,7 +132,6 @@ class HomeActivity : ComponentActivity() {
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             val showGirls by homeViewModel.showGirls.collectAsState()
-                            val currentDestination by homeViewModel.currentDestinationBottomNav.collectAsState()
                             BottomNav(
                                 items = bottomBarItems,
                                 navController = navController
@@ -136,14 +142,90 @@ class HomeActivity : ComponentActivity() {
                     },
                     containerColor = MaterialTheme.colorScheme.background
                 ) { innerPadding ->
-                    val scaffoldSheetState = rememberBottomSheetScaffoldState()
+
+                    val sheetState = rememberStandardBottomSheetState(
+//                        initialValue = SheetValue.PartiallyExpanded,
+                        skipHiddenState = false
+                    )
+                    val scaffoldState = rememberBottomSheetScaffoldState(
+                        bottomSheetState = sheetState
+                    )
+                    val scope = rememberCoroutineScope()
+
                     val bottomPadding = innerPadding.calculateBottomPadding() + 40.dp
 
-                    BottomSheetScaffoldForHome(
-                        modifier = Modifier.padding(innerPadding),
-                        scaffoldSheetState = scaffoldSheetState,
-                        bottomPadding = bottomPadding
+                    val showResults by homeViewModel.showSearchResults.collectAsState()
+
+
+                    BottomSheetScaffold(
+                        modifier = Modifier,
+                        scaffoldState = scaffoldState,
+                        sheetPeekHeight = if (currentDestination == "home") {
+                            bottomPadding
+                        } else 0.dp,
+                        sheetContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        sheetContent = {
+                            Surface(color = MaterialTheme.colorScheme.primaryContainer) {
+                                Column {
+                                    var showSingleGirlView by rememberSaveable {
+                                        mutableStateOf(false)
+                                    }
+                                    Row {
+                                        IconButton(
+                                            onClick = {
+                                                showSingleGirlView = !showSingleGirlView
+                                            }) {
+                                            Icon(
+                                                modifier = Modifier.size(32.dp),
+                                                painter = painterResource(id = R.drawable.icon_cards),
+                                                contentDescription = "Cards Icon"
+                                            )
+                                        }
+                                        Spacer(modifier = Modifier.weight(1f))
+
+                                    }
+                                    AnimatedVisibility(
+                                        visible = showSingleGirlView,
+                                        enter = scaleIn(transformOrigin = TransformOrigin(0f, 0f)),
+                                        exit = scaleOut(transformOrigin = TransformOrigin(0f, 0f))
+                                    ) {
+                                        val pagerState = rememberPagerState(pageCount = { 3 })
+
+                                        HorizontalPager(
+                                            modifier = Modifier.padding(bottom = bottomPadding),
+                                            state = pagerState
+                                        ) {
+                                            SingleGirlView(
+                                                modifier = Modifier
+                                                    .fillMaxSize()
+                                                    .padding(horizontal = 40.dp, vertical = 20.dp),
+                                                imageModifier = Modifier.fillMaxHeight()
+                                            )
+                                        }
+                                    }
+                                    AnimatedVisibility(
+                                        visible = !showSingleGirlView,
+                                    ) {
+                                        AvailableGirlsVerticalGrid(
+                                            modifier = Modifier.padding(
+                                                start = 20.dp,
+                                                end = 20.dp
+                                            )
+                                        )
+                                    }
+
+                                }
+                            }
+                        }
                     ) {
+                        LaunchedEffect(currentDestination) {
+                            scope.launch {
+                                if (currentDestination != "home")
+                                    if (sheetState.hasExpandedState) {
+                                        sheetState.hide()
+                                    }
+                            }
+                        }
                         Surface(
                             modifier = Modifier
                                 .fillMaxSize()
@@ -157,74 +239,6 @@ class HomeActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
-@Composable
-fun BottomSheetScaffoldForHome(
-    modifier: Modifier = Modifier,
-    scaffoldSheetState: BottomSheetScaffoldState,
-    bottomPadding: Dp,
-    content: @Composable() (() -> Unit)
-) {
-    BottomSheetScaffold(
-        modifier = modifier,
-        scaffoldState = scaffoldSheetState,
-        sheetPeekHeight = bottomPadding,
-        sheetContainerColor = MaterialTheme.colorScheme.primaryContainer,
-        sheetContent = {
-            Surface(color = MaterialTheme.colorScheme.primaryContainer) {
-                Column {
-                    var showSingleGirlView by rememberSaveable {
-                        mutableStateOf(false)
-                    }
-                    Row {
-                        IconButton(
-                            onClick = { showSingleGirlView = !showSingleGirlView }) {
-                            Icon(
-                                modifier = Modifier.size(32.dp),
-                                painter = painterResource(id = R.drawable.icon_cards),
-                                contentDescription = "Cards Icon"
-                            )
-                        }
-                        Spacer(modifier = Modifier.weight(1f))
-
-                    }
-                    AnimatedVisibility(
-                        visible = showSingleGirlView,
-                        enter = scaleIn(transformOrigin = TransformOrigin(0f, 0f)),
-                        exit = scaleOut(transformOrigin = TransformOrigin(0f, 0f))
-                    ) {
-                        val pagerState = rememberPagerState(pageCount = { 3 })
-
-                        HorizontalPager(
-                            modifier = Modifier.padding(bottom = bottomPadding),
-                            state = pagerState
-                        ) {
-                            SingleGirlView(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(horizontal = 40.dp, vertical = 20.dp),
-                                imageModifier = Modifier.fillMaxHeight()
-                            )
-                        }
-                    }
-                    AnimatedVisibility(
-                        visible = !showSingleGirlView,
-                    ) {
-                        AvailableGirlsVerticalGrid(
-                            modifier = Modifier.padding(
-                                start = 20.dp,
-                                end = 20.dp
-                            )
-                        )
-                    }
-
-                }
-            }
-        }
-    ) {
-        content()
-    }
-}
 
 @Composable
 fun NavigationForHome(
@@ -251,7 +265,11 @@ fun NavigationForHome(
         }
         composable("status") {
             homeViewModel.updateCurrentDestinationBottomNav("status")
-            RequestReceivedScreen(modifier = Modifier.verticalScroll(rememberScrollState()))
+            RequestReceivedScreen(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize()
+            )
         }
         composable("profile") {
             homeViewModel.updateCurrentDestinationBottomNav("profile")
