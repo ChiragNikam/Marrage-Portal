248c20e9640601a7fdfd939dc5337be0fc773543
Rajat Kumar Singh <134076578+rajatkumarsingh01@users.noreply.github.com>
2024-03-06 12:02:32 +0530

    api - setup for getUserData

--- app/src/main/java/com/petukji/marrageportal/ApiService.kt
+++ app/src/main/java/com/petukji/marrageportal/ApiService.kt
@@ -1,5 +1,6 @@
 package com.petukji.marrageportal
 
+import com.petukji.marrageportal.bottom_nav.data.UserProfile
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -7,4 +8,8 @@ import retrofit2.http.POST
 interface ApiService {
     @POST("/getUserPreference")
     fun getUserPreference(): Call<userPreference>
+
+    @POST("/getUserData")
+    fun getUserData():Call<UserProfile>
+
 }
