2ebee8811bcfa0c47c081897e4264b486f93d0f7
Sourabh Kumar <sourabh.47512@gmail.com>
2024-03-14 01:52:30 +0530

    feature - added ForWhomScreen(registration 1st page)

---
+++ app/src/main/java/com/petukji/marrageportal/auth/data/SelectionType.kt
@@ -0,0 +1,8 @@
+package com.petukji.marrageportal.auth.data
+
+enum class SelectionType {
+    MySelf,
+    Child,
+    GrandChild,
+    Others
+}
---
+++ app/src/main/java/com/petukji/marrageportal/auth/presentation/RegistrationScreen.kt
@@ -0,0 +1,64 @@
+package com.petukji.marrageportal.auth.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.petukji.marrageportal.R
+import com.petukji.marrageportal.auth.data.SelectionType
+
+@Composable
+fun RegistrationScreen() {
+    ForWhomScreen()
+}
+
+@Composable
+fun ForWhomScreen() {
+    Column(
+        modifier = Modifier.padding(top = 210.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "For whom are you creating?",
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        SelectionType.entries.forEach { groom ->
+            GradientButton(
+                text = groom.name
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                colorResource(id = R.color.lightRed)
+            ),
+            onClick = { }
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegistrationScreenPreview() {
+    RegistrationScreen()
+}
